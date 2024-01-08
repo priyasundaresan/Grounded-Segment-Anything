083f9a31116e470cb5680493e580c603b39cf888
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2024-01-08 15:09:32 -0800

    ok

--- inference_class.py
+++ inference_class.py
@@ -58,24 +58,6 @@ class BiteAcquisitionInference:
         chatbot_response = response.choices[0].message.content
         return chatbot_response.strip()
 
-    def rgbdCallback(self, rgb_image_msg, camera_info_msg, depth_image_msg):
-        try:
-            # Convert your ROS Image message to OpenCV2
-            rgb_image = self.bridge.imgmsg_to_cv2(rgb_image_msg, "bgr8")
-            depth_image = self.bridge.imgmsg_to_cv2(depth_image_msg, "32FC1")
-        except CvBridgeError as e:
-            print(e)
-
-        with self.camera_lock:
-            self.camera_header = rgb_image_msg.header
-            self.camera_color_data = rgb_image
-            self.camera_info_data = camera_info_msg
-            self.camera_depth_data = depth_image
-
-    def get_camera_data(self):
-        with self.camera_lock:
-            return self.camera_header, self.camera_color_data, self.camera_info_data, self.camera_depth_data
-
     def detect_items(self, image):
         # detect objects
         detections = self.grounding_dino_model.predict_with_classes(
